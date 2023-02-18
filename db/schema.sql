2c74ba7d8cbef629dc7636aa003ef7b799aee519
tesseir <tesseir.edu@gmail.com>
2023-02-17 22:57:16 -0600

    fixed db

--- db/schema.sql
+++ db/schema.sql
@@ -13,17 +13,19 @@ CREATE TABLE jobPosition (
   id int auto_increment primary key,
   pos_name varchar(100),
   salary decimal,
-  dep_id int not null,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  foreign key (dep_id) references department(id)
+  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE employee (
   id int auto_increment primary key,
+  jobPosition_id int,
   first_name varchar(30),
   last_inital varchar(1),
   pos_id int not null,
+  dep_id int not null,
   manager_id int,
   date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  foreign key (jobPosition_id) references jobPosition(id)
+  foreign key (jobPosition_id) references jobPosition(id),
+  foreign key (dep_id) references department(id),
+  foreign key (manager_id) references employee(id)
 );
